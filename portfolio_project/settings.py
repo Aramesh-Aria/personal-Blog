53db15e248c8f4945201bb50e92f1f609f5ccc4b
Aria Aramesh <arya.aeamesh@gmail.com>
2026-05-24 15:22:53 +0330

    ✦ chore: update static file configuration and gitignore
       - Update .gitignore to exclude static_dev/, db.sqlite3, and database/ directory.
       - Update STATIC_URL in settings.py to absolute path.
       - Enable and update STATICFILES_DIRS to use static_dev directory.

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'public/static/'
+STATIC_URL = '/public/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "public","static")
 
-# STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static_dev']
 
 MEDIA_URL = '/public/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "public","media")
